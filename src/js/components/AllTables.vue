79e2ca502f6744e672dd258fab03ee6f448af686
lahin31 <muhammad.lahin@gmail.com>
2018-07-10 09:30:54 +0600

    Updating editor component

---
+++ src/js/components/AllTables.vue
@@ -0,0 +1,156 @@
+<template>
+    <div class="editor">
+        <div class="editor_header">
+            <div class="section_header">
+                <h1>All Mortgage Tables</h1>
+            </div>
+            <div class="section_action">
+                <el-button size="mini" type="primary" @click="addTableModal = true">Add Table</el-button>
+            </div>
+        </div>
+    
+        <hr>
+
+
+        <el-table :data="tableData"
+                  style="width: 100%; margin-top: 10px;">
+            
+            <el-table-column 
+                prop="id"
+                label="ID"
+                width="60">
+            </el-table-column>
+
+            <el-table-column label="Name">
+                <template slot-scope="scope">
+                    {{ scope.row.name }}
+                </template>
+            </el-table-column>
+
+            <el-table-column 
+                label="ShortCode">
+                <template slot-scope="scope">
+                    {{ scope.row.shortCode }}
+                </template>
+            </el-table-column>
+
+            <el-table-column  label="Actions" width="190">
+                <template slot-scope="scope">
+                    <i class="el-icon-edit"></i>
+                    <a href=""  target="_blank" class="el-button el-button--info el-button--mini">
+                        <i class="el-icon-view"></i>
+                    </a>
+                </template>
+            </el-table-column>
+
+
+        </el-table>
+
+        <!-- Dialog -->
+        <el-dialog
+            title="Add New Pricing Table"
+            :visible.sync="addTableModal"
+            width="60%">
+                <label for="new_table_name">Table Name</label>
+                <el-input id="new_table_name" type="text" placeholder="Your Table Name" v-model="table_name"></el-input>
+
+                <el-select v-model="selected_table_type" placeholder="Select Calculator Type" class="select_calc_type">
+                    
+                    <el-option
+                        v-for="(item, i) in table_type"
+                        :key="i"
+                        :label="item.label"
+                        :value="item.value">
+                    </el-option>
+                
+                </el-select>
+
+                <span slot="footer" class="dialog-footer">
+
+                    <el-button @click="addTableModal = false">Cancel</el-button>
+                    <el-button type="primary" @click="addNewTable">Add New</el-button>
+
+                </span>
+            </el-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'all_mortgage_tables',
+    data() {
+        return {
+            addTableModal: false,
+            table_type: [
+                {
+                    value: 'mortgage_calculator',
+                    label: 'Mortgage Calculator'
+                }, 
+                {
+                    value: 'mortgage_refinance',
+                    label: 'Mortgage Refinance Calculator'
+                }, 
+                {
+                    value: 'mortgage_payment',
+                    label: 'Mortgage Payment Calculator'
+                }
+            ],
+            tableData: [],
+            selected_table_type: '',
+            table_name: ''
+        }
+    },
+    methods: {
+
+        addNewTable() {
+
+            this.tableData.push({
+
+                id: '20',
+                name: this.table_name,
+                shortCode: '[wp_price_table id="1070"]'
+
+            });
+
+            this.addTableModal = false;
+
+        }
+
+    }
+}
+</script>
+
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        background: #F1F1F1;
+    }
+
+    /* .section_header {
+        float: left;
+    } */
+
+    .section_action {
+        float: right;
+        margin-top: -32px;
+    }
+
+    .select_calc_type {
+        margin-top: 10px;
+    }
+
+    .editor {
+        width: 100%;
+        float: right;
+    }
+
+    hr {
+        color: #DDDDDD;
+    }
+</style>
---
+++ src/js/ninja_mortgage_calc.js
@@ -0,0 +1,62 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import ElementUI from 'element-ui';
+
+import routes from './router.js';
+
+import locale from 'element-ui/lib/locale';
+import lang from 'element-ui/lib/locale/lang/en';
+locale.use(lang);
+Vue.use(VueRouter);
+
+import { 
+	Button, 
+	Dialog, 
+	Input, 
+	Select, 
+	Option,
+	Table, 
+	Icon,
+	TableColumn } from 'element-ui';
+
+import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/table.css';
+import 'element-ui/lib/theme-chalk/table-column.css';
+import 'element-ui/lib/theme-chalk/color-picker.css';
+import 'element-ui/lib/theme-chalk/button-group.css';
+import 'element-ui/lib/theme-chalk/button.css';
+import 'element-ui/lib/theme-chalk/switch.css';
+import 'element-ui/lib/theme-chalk/select-dropdown.css';
+import 'element-ui/lib/theme-chalk/option.css';
+import 'element-ui/lib/theme-chalk/select.css';
+import 'element-ui/lib/theme-chalk/input.css';
+import 'element-ui/lib/theme-chalk/dialog.css';
+import 'element-ui/lib/theme-chalk/notification.css';
+import 'element-ui/lib/theme-chalk/loading.css';
+import 'element-ui/lib/theme-chalk/popover.css';
+import 'element-ui/lib/theme-chalk/pagination.css';
+
+
+Vue.use(Button)
+Vue.use(Dialog)
+Vue.use(Input)
+Vue.use(Select)
+Vue.use(Option)
+Vue.use(Table)
+Vue.use(Icon)
+Vue.use(TableColumn)
+
+
+const router = new VueRouter({
+	routes
+});
+
+import App from './AdminApp';
+
+new Vue({
+    el:'#ninja_mortgage_calc',
+    router,
+    render: h => h(App)
+});
+
+console.log("JS rendered successfully");
