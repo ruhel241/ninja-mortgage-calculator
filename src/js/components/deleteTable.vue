59c7eb853d5b51ae2cb8f91aeafd228476df09d3
MuhammadLahin <muhammad.lahin@gmail.com>
2018-07-11 10:34:11 +0600

    Update deleteTable.vue

--- src/js/components/deleteTable.vue
+++ src/js/components/deleteTable.vue
@@ -2,8 +2,8 @@
     <el-popover placement="top" width="160" style="margin-left: 10px" v-model="isVisible">
         <p>Are you sure to delete this?</p>
         <div style="text-align: right; margin: 0">  
-            <el-button size="mini" type="text" @click="isVisible=false">cancel</el-button>
-            <el-button size="mini" type="primary" @click="confirm()">confirm</el-button> 
+            <el-button size="mini" type="text" @click="isVisible=false">Cancel</el-button>
+            <el-button size="mini" type="primary" @click="confirm()">Confirm</el-button> 
         </div>
         <el-button slot="reference" title="Delete" size="mini" type="danger" icon="el-icon-delete"></el-button>
     </el-popover>
