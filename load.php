e3d0975bc114c8f7761756d2a720c76d64036092
Ruhel <ruhel241@gmail.com>
2018-07-19 19:51:50 +0600

    refactored

--- load.php
+++ load.php
@@ -3,7 +3,4 @@
 include "Classes/CPT.php";
 include "Classes/Menu.php";
 include "Classes/MortgageCalculatorHandler.php";
-include "Classes/CalculatorRenderer.php";
-include "Classes/View.php";
-include "Classes/global_functions.php";
 include "Classes/ProcessDemoPage.php";
